43a9d893522af2c56c0e38bbdeba6ea6d090dd54
yinxuanwang <64998199+yinxuanwang@users.noreply.github.com>
2026-02-12 10:36:53 -0500

    lab02

---
+++ lab02/bootstrap_starter.R
@@ -0,0 +1,20 @@
+make_clustered_data <- function(G, N) {
+    g <- sample(1:G, size = N, replace = T)
+    treat.g <- sample(c(0, 1), size = G, replace = T)
+    treat.i <- sapply(g, \(x) treat.g[x])
+    effect.g <- rnorm(1:G)
+    g.i <- sapply(g, \(x) effect.g[x])
+    y <- rnorm(N) + g.i + 0.5 * treat.i
+    data.frame(
+        g = g,
+        treat = treat.i,
+        y = y
+    )
+}
+
+set.seed(123)
+df <- make_clustered_data(10, 100)
+model <- lm(y ~ treat, df)
+summary(model)
+
+###
